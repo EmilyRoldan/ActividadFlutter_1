990bf93d5dfa1738ae6f819b3aa76e7d4eaf450c
EmilyRoldan <98895093+EmilyRoldan@users.noreply.github.com>
2025-02-12 11:45:39 -0500

    Update flutter_course_page.dart

--- lib/ui/pages/flutter_course_page.dart
+++ lib/ui/pages/flutter_course_page.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-
 
 class FlutterCoursePage extends StatelessWidget {
   const FlutterCoursePage({super.key});
@@ -8,194 +6,267 @@ class FlutterCoursePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        buildModules(),
-      ],
+      children: [header(), buildModules()],
     );
   }
 
-  Widget buildModules() {
+  Widget header() {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        Divider(),
-        Center(
-            child: Text(
-          "Modules",
-          style: const TextStyle(
+        const Text(
+          'Course focus',
+          textAlign: TextAlign.left,
+          style: TextStyle(
             fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 0, 0, 0),
+            fontSize: 20.0,
           ),
-        )),
-        SingleChildScrollView(
-          padding: EdgeInsets.only(top: 30),
-          child: Expanded(
+        ),
+        Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Container(
+                    color: Color.fromARGB(199, 51, 9, 140),
+                    height: 5,
+                  ),
+                  const Text(
+                    'UI Development',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 10.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Container(color: Color.fromARGB(199, 245, 10, 10), height: 5),
+                  const Text(
+                    'Architecture',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 10.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Container(
+                      color: Color.fromARGB(199, 228, 251, 19), height: 5),
+                  const Text(
+                    'Desing Testing Thinking',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 10.0,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+Widget buildModules() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: [
+      Divider(),
+      Center(
+          child: Text(
+        "Modules",
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      )),
+      SingleChildScrollView(
+        padding: EdgeInsets.only(top: 30),
+        child: Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: ContainerIconwithText(
+                  color: Colors.blue,
+                  icon: Icons.book_sharp,
+                  label: 'Introduction',
+                ),
+              ),
+              Expanded(
+                  child: ContainerIconwithText(
+                      color: Colors.red,
+                      icon: Icons.design_services,
+                      label: "UX design")),
+              Expanded(
+                  child: ContainerIconwithText(
+                      color: Colors.orange,
+                      icon: Icons.storage,
+                      label: "State Management")),
+              Expanded(
+                  child: ContainerIconwithText(
+                      color: Colors.grey,
+                      icon: Icons.bug_report,
+                      label: "Testing")),
+              Expanded(
+                  child: ContainerIconwithText(
+                      color: Colors.blue,
+                      icon: Icons.network_check,
+                      label: "Networking")),
+            ],
+          ),
+        ),
+      ),
+      Text(""),
+      Divider()
+    ],
+  );
+}
+
+Widget buildProjectList() {
+  return Expanded(
+    child: ListView(
+      padding: const EdgeInsets.all(12),
+      children: <Widget>[
+        Container(
+          height: 50,
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 214, 213, 211),
+            border: Border.all(
+                color: const Color.fromARGB(255, 242, 240, 240), width: 1),
+            borderRadius: BorderRadius.circular(7),
+          ),
+          child: const Center(
             child: Row(
               children: [
-                Expanded(
-                  child: ContainerIconwithText(
-                    color: Colors.blue,
-                    icon: Icons.book_sharp,
-                    label: 'Introduction',
-                  ),
+                Icon(Icons.folder, color: Colors.blueAccent),
+                const Text('  Sudoku'),
+                Spacer(flex: 2),
+                Icon(
+                  Icons.more_vert_rounded,
                 ),
-                Expanded(
-                    child: ContainerIconwithText(
-                        color: Colors.red,
-                        icon: Icons.design_services,
-                        label: "UX design")),
-                Expanded(
-                    child: ContainerIconwithText(
-                        color: Colors.orange,
-                        icon: Icons.storage,
-                        label: "State Management")),
-                Expanded(
-                    child: ContainerIconwithText(
-                        color: Colors.grey,
-                        icon: Icons.bug_report,
-                        label: "Testing")),
-                Expanded(
-                    child: ContainerIconwithText(
-                        color: Colors.blue,
-                        icon: Icons.network_check,
-                        label: "Networking")),
               ],
             ),
           ),
         ),
-        Text(""),
-        Divider()
+        Container(
+          height: 50,
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 214, 213, 211),
+            border: Border.all(
+                color: const Color.fromARGB(255, 242, 240, 240), width: 1),
+            borderRadius: BorderRadius.circular(7),
+          ),
+          child: const Center(
+            child: Row(
+              children: [
+                Icon(Icons.folder, color: Colors.blueAccent),
+                const Text('  Random user'),
+                Spacer(flex: 2),
+                /*3*/
+                Icon(
+                  Icons.more_vert_rounded,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          height: 50,
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 214, 213, 211),
+            border: Border.all(
+                color: const Color.fromARGB(255, 242, 240, 240), width: 1),
+            borderRadius: BorderRadius.circular(7),
+          ),
+          child: const Center(
+            child: Row(
+              children: [
+                Icon(Icons.folder, color: Colors.blueAccent),
+                const Text('  Note Taking'),
+                Spacer(flex: 2),
+                /*3*/
+                Icon(
+                  Icons.more_vert_rounded,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          height: 50,
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 214, 213, 211),
+            border: Border.all(
+                color: const Color.fromARGB(255, 242, 240, 240), width: 1),
+            borderRadius: BorderRadius.circular(7),
+          ),
+          child: const Center(
+            child: Row(
+              children: [
+                Icon(Icons.folder, color: Colors.blueAccent),
+                const Text('  Weather'),
+                Spacer(flex: 2),
+                /*3*/
+                Icon(
+                  Icons.more_vert_rounded,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          height: 50,
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 214, 213, 211),
+            border: Border.all(
+                color: const Color.fromARGB(255, 242, 240, 240), width: 1),
+            borderRadius: BorderRadius.circular(7),
+          ),
+          child: const Center(
+            child: Row(
+              children: [
+                Icon(Icons.folder, color: Colors.blueAccent),
+                const Text('  Delivery App'),
+                Spacer(flex: 2),
+                /*3*/
+                Icon(
+                  Icons.more_vert_rounded,
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
-    );
-  }
-
-  Expanded buildProjectList() {
-    return Expanded(
-      child: ListView(
-        padding: const EdgeInsets.all(12),
-        children: <Widget>[
-          Container(
-            height: 50,
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 214, 213, 211),
-              border: Border.all(
-                  color: const Color.fromARGB(255, 242, 240, 240), width: 1),
-              borderRadius: BorderRadius.circular(7),
-            ),
-            child: const Center(
-              child: Row(
-                children: [
-                  Icon(Icons.folder, color: Colors.blueAccent),
-                  const Text('  Sudoku'),
-                  Spacer(flex: 2),
-                  Icon(
-                    Icons.more_vert_rounded,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: 50,
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 214, 213, 211),
-              border: Border.all(
-                  color: const Color.fromARGB(255, 242, 240, 240), width: 1),
-              borderRadius: BorderRadius.circular(7),
-            ),
-            child: const Center(
-              child: Row(
-                children: [
-                  Icon(Icons.folder, color: Colors.blueAccent),
-                  const Text('  Random user'),
-                  Spacer(flex: 2),
-                  /*3*/
-                  Icon(
-                    Icons.more_vert_rounded,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: 50,
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 214, 213, 211),
-              border: Border.all(
-                  color: const Color.fromARGB(255, 242, 240, 240), width: 1),
-              borderRadius: BorderRadius.circular(7),
-            ),
-            child: const Center(
-              child: Row(
-                children: [
-                  Icon(Icons.folder, color: Colors.blueAccent),
-                  const Text('  Note Taking'),
-                  Spacer(flex: 2),
-                  /*3*/
-                  Icon(
-                    Icons.more_vert_rounded,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: 50,
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 214, 213, 211),
-              border: Border.all(
-                  color: const Color.fromARGB(255, 242, 240, 240), width: 1),
-              borderRadius: BorderRadius.circular(7),
-            ),
-            child: const Center(
-              child: Row(
-                children: [
-                  Icon(Icons.folder, color: Colors.blueAccent),
-                  const Text('  Weather'),
-                  Spacer(flex: 2),
-                  /*3*/
-                  Icon(
-                    Icons.more_vert_rounded,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: 50,
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 214, 213, 211),
-              border: Border.all(
-                  color: const Color.fromARGB(255, 242, 240, 240), width: 1),
-              borderRadius: BorderRadius.circular(7),
-            ),
-            child: const Center(
-              child: Row(
-                children: [
-                  Icon(Icons.folder, color: Colors.blueAccent),
-                  const Text('  Delivery App'),
-                  Spacer(flex: 2),
-                  /*3*/
-                  Icon(
-                    Icons.more_vert_rounded,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+    ),
+  );
 }
 
 class ContainerIconwithText extends StatelessWidget {
